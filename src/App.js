6558fb3db91a4e93b7e15d5614951f41242507fa
thi131190 <thi131190@gmail.com>
2019-10-16 13:41:49 +0700

    Complete milestone 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className='container-fluid text-white my-auto'>
+        <div className='container mx-auto my-4 py-4'>
+          <div className='row justify-content-center text-center'>
+            <h1 className='col-12 display-4 my-2 py-3 text-success'>
+              Awesome Weather App
+            </h1>
+            <h2 className='col-12'>Location Name</h2>
+            <h3 className='col-12 text-danger'>Temperature</h3>
+            <h3 className='col-12'>Weather Description</h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
